379dd22cad123e94ecc9d7df23f8a881072b6ffb
John Wells <molekewl2017@gmail.com>
2019-09-12 20:19:25 -0700

    style colors & spinner color

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
   return (
     <div className='loader center'>
-    <i className="fa fa-cog fa-spin fa-7x" />
+    <i className="fa fa-spinner fa-spin fa-7x" />
     </div>
   );
 }
